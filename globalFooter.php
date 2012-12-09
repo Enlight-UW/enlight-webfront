ad8dfa80ec6faf2a2dec1b908feb84ee40b8c750
Alex Kersten <alex@dividebyxero.com>
2012-12-08 21:17:24 -0600

    Little bit of version and credits info in the footer.

--- globalFooter.php
+++ globalFooter.php
@@ -1,13 +1,16 @@
 <?php
-
 /*
  * Project: CleanWebfront
  * File: globalFooter.php
  * Author: Alex Kersten
  * 
  * Really all we do here is close tags opened by globalHeader.php.
+ * Also the credits are here.
  */
 ?>
-
+        <div class="muted" style="text-align: center;">
+            <!-- Add your name! -->
+            <abbr title="&copy; 2012 Enlight (Alex Kersten, )">Webfront 0.0.5</abbr>
+        </div>
     </body>
 </html>
