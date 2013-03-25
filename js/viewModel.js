788c434c2aca138c9fd4d9415e8e8be5699cb39c
Alex Kersten <alex@dividebyxero.com>
2013-03-25 04:31:00 -0500

    Missed this file last time

---
+++ js/viewModel.js
@@ -0,0 +1,9 @@
+//This is the Knockout viewmodel that all dynamic elements of the page will
+//reference - this gets updated by the KO Mapping plugin - we'll just define
+//the default values here.
+var defaultData = {
+    error: false,
+    errormessage: "No error",
+    fountainState: "Initializing Webfront...",
+    valveState: 0
+}
