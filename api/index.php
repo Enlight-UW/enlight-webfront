1f4aa8e5def1738af2efd39d4f5772818e8c1eab
Alex Kersten <alex@dividebyxero.com>
2014-07-21 09:48:03 -0500

    add api key verification and priority checking

--- api/index.php
+++ api/index.php
@@ -16,6 +16,46 @@ $app->response->headers->set('Content-Type', 'application/json');
 // Initialize database connection, we'll probably need it.
 $db = new SQLite3('../' . $DB_FILENAME);
 
+// Will move these functions somewhere else eventually.
+function verifyAPIKey($key) use ($db) {
+    $Q_QUERY_API_KEY_COUNT = <<<stmt
+        SELECT COUNT(*) as count FROM apikeys
+        WHERE apikey=:key
+stmt;
+
+    $stmt = $db->prepare($Q_QUERY_API_KEY_COUNT);
+    $stmt->bindValue(':key', $key, SQLITE3_STRING)
+    $res = $stmt->execute();
+
+    //TODO: Error check
+    
+    // If there are any returned, it's a valid key
+    $r = $res->fetchArray();
+    return $r['count'] > 0;
+}
+
+function getAPIKeyPriority($key) use ($db) {
+    if (!verifyAPIKey($key))
+        return 0;
+
+    $Q_QUERY_API_KEY_PRIORITY = <<<stmt
+        SELECT priority FROM apikeys
+        WHERE apikey=:key
+stmt;
+
+    $stmt = $db->prepare($Q_QUERY_API_KEY_PRIORITY);
+    $stmt->bindValue(':key', $key, SQLITE3_STRING)
+    //TODO: Error check
+    $res = $stmt->execute();
+    
+    $r = $res->fetchArray();
+
+    return $r['priority'];
+    
+}
+
+///////////////////////////////////////////////////////
+
 // Define routes.
 
 /**
