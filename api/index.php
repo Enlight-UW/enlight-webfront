14fbda3c5027c0ebd5e0e510ab0c7fd9ac3855a9
Alex Kersten <alex@dividebyxero.com>
2014-07-21 11:05:21 -0500

    take these functions...

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require '../constants.php';
+require 'apiFunctions.php';
 
 // Initialize Slim REST framework.
 require 'Slim/Slim.php';
@@ -15,46 +16,6 @@ $app->response->headers->set('Content-Type', 'application/json');
 
 // Initialize database connection, we'll probably need it.
 $db = new SQLite3('../' . $DB_FILENAME);
-
-// Will move these functions somewhere else eventually.
-function verifyAPIKey($key) use ($db) {
-    $Q_QUERY_API_KEY_COUNT = <<<stmt
-        SELECT COUNT(*) as count FROM apikeys
-        WHERE apikey=:key
-stmt;
-
-    $stmt = $db->prepare($Q_QUERY_API_KEY_COUNT);
-    $stmt->bindValue(':key', $key, SQLITE3_STRING)
-    $res = $stmt->execute();
-
-    //TODO: Error check
-    
-    // If there are any returned, it's a valid key
-    $r = $res->fetchArray();
-    return $r['count'] > 0;
-}
-
-function getAPIKeyPriority($key) use ($db) {
-    if (!verifyAPIKey($key))
-        return 0;
-
-    $Q_QUERY_API_KEY_PRIORITY = <<<stmt
-        SELECT priority FROM apikeys
-        WHERE apikey=:key
-stmt;
-
-    $stmt = $db->prepare($Q_QUERY_API_KEY_PRIORITY);
-    $stmt->bindValue(':key', $key, SQLITE3_STRING)
-    //TODO: Error check
-    $res = $stmt->execute();
-    
-    $r = $res->fetchArray();
-
-    return $r['priority'];
-    
-}
-
-///////////////////////////////////////////////////////
 
 // Grab the API key from this request if it's a POST, and verify this API key.
 if ($app->request->isPost()) {
