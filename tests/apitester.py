63f2eedffb1ed94178284e168d0b2c0b1eee81a4
Alex Kersten <alex@dividebyxero.com>
2014-07-27 00:24:34 -0500

    python api test

---
+++ tests/apitester.py
@@ -0,0 +1,22 @@
+import urllib
+import urllib2
+import time
+import json
+
+i = 1
+while True:
+	i = i << 1
+	if i > (1 << 24):
+		i = 1
+		
+	data = {
+			'apikey': 'abc123',
+			'bitmask': i
+	}
+
+	req = urllib2.Request('http://alexkersten.com/enlight/api/valves')
+	req.add_header('Content-Type', 'application/json')
+	response = urllib2.urlopen(req, json.dumps(data))
+	print response;
+
+	time.sleep(1)
